e512ccbe12980f1ab8b5da8db53360ee023a9385
Joachim Arrasz <arrasz@synyx.de>
2018-05-31 14:48:09 +0200

    Refactored and renamed

---
+++ src/main/org/synyx/DemoApplication.java
@@ -0,0 +1,22 @@
+package org.synyx;
+
+
+import io.micronaut.runtime.Micronaut;
+
+/*
+ * 31.05.2018
+ */
+
+/**
+ * DeomApplication starter. 
+ * 
+ * @author Joachim Arrasz <arrasz@synyx.de>
+ */
+
+public class DemoApplication {
+    
+    public static void main(String[] args) {
+        Micronaut.run(DemoApplication.class);
+    }
+    
+}
--- src/test/java/DemoController.java
+++ src/test/java/DemoController.java
@@ -1,22 +1,22 @@
-/*
- * 31.05.2018
- */
-package micronaut.jooq.demo;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 
+/*
+ * 31.05.2018
+*/
+
 /**
- * Controller for all demo purposes.
- *
+ * Controller for demo purposes.
+ * 
  * @author Joachim Arrasz <arrasz@synyx.de>
  */
 @Controller("/")
 public class DemoController {
-
+    
     @Get("/")
-    public String index() {
-        return "Hi! I'm your Democontroller";
+    public String getHello() {
+        return "Hi! I'm your DemoController!";
     }
-
+    
 }
